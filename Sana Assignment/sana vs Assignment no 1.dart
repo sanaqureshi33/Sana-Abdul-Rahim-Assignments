4d014b6b6126d093f66a076ddbccf13c9dc666ba
sanaqureshi33 <94166252+sanaqureshi33@users.noreply.github.com>
2021-11-15 23:07:00 +0500

    work complete

---
+++ Sana Assignment/sana vs Assignment no 1.dart
@@ -0,0 +1,148 @@
+import 'dart:io';
+
+void main()
+{
+  //       QUESTION NO 1
+// stdout.write("Enter Visitor Name");
+// var name = stdin.readLineSync();
+
+// stdout.write("Enter Quantity");
+// var quantity = stdin.readLineSync();
+
+// stdout.write("Enter Product");
+// var product = stdin.readLineSync();
+// print("${name} ordered ${quantity}  ${product} on XYZ clothing store");
+  
+
+     //QUESTION NO 2
+// stdout.write("Enter Number ");
+// int? v1 =int.parse(stdin.readLineSync()!);
+
+// stdout.write("Enter Number ");
+// int? v2 =int.parse(stdin.readLineSync()!);
+//  int res = v1-v2;
+//  print("Subtraction is =${res}"); 
+
+//QUESTION NO 3
+
+   // (Subtraction)
+// stdout.write("Enter Number ");
+// int? v1 =int.parse(stdin.readLineSync()!);
+
+// stdout.write("Enter Number ");
+// int? v2 =int.parse(stdin.readLineSync()!);
+//  print("Subtraction is =${v1-v2}");
+
+  //(Addition)
+// stdout.write("Enter Number ");
+// int? v1 =int.parse(stdin.readLineSync()!);
+
+// stdout.write("Enter Number ");
+// int? v2 =int.parse(stdin.readLineSync()!);
+//  print("Addition is =${v1+v2}");
+ 
+   //(Multiplication)
+// stdout.write("Enter Number ");
+// int? v1 =int.parse(stdin.readLineSync()!);
+
+// stdout.write("Enter Number ");
+// int? v2 =int.parse(stdin.readLineSync()!);
+//  print("Multiplcation is =${v1*v2}");
+
+
+   //(Modulus)
+stdout.write("Enter Number ");
+int? v1 =int.parse(stdin.readLineSync()!);
+
+stdout.write("Enter Number ");
+int? v2 =int.parse(stdin.readLineSync()!);
+ print("Modulus is =${v1~/v2}");
+
+
+
+//QUESTION NO 4
+
+
+// int tk= 600;
+// int a1 =int.parse(stdin.readLineSync()!);
+// print(tk*a1);
+
+// int Ticket_Price = 600;
+// print("Ticket_Price=600");
+// stdout.write("Enter No Of Tickets=");
+// int No_Of_Ticket = int.parse(stdin.readLineSync()!);
+
+// print(Ticket_Price*No_Of_Ticket);
+
+
+//Question No 5
+
+// stdout.write("Enter Celsius Temperature = ");
+// double T1 = double.parse(stdin.readLineSync()!);
+// print("F =${(T1*9/5)+32}");
+
+//QUESION NO 6
+
+// int Price_of_item_1 = 20;
+// int Price_of_item_2 = 20;
+// print("Price_of_item_1 = 20");
+// stdout.write("Enter Quantity of item 1");
+// int a1 = int.parse(stdin.readLineSync()!);
+// print(Price_of_item_1*a1);
+// print("Price_of_item_2= 20" );
+// stdout.write("Enter Quantity of item 2");
+// int a2 = int.parse(stdin.readLineSync()!);
+// print(Price_of_item_2*a2);
+
+ //QUESTION NO 7
+// int s1 =20;
+// int s2 =30;
+// int t1 = 40;
+
+// print("Total Marks=${s1+s2}");
+// print("Student1 percentage is = ");
+// print(s1/t1*100);
+// print("Student2 percentage is = ");
+// print(s2/t1*100);
+
+//QUESTION NO 8
+// num us =175.20;
+// num ri =46.85;
+// print("Price of 10 us dollar in Pak Rupees is =${us*10} Price of 25 Saudi Riyal in Pak Rupees is = ${ri*25}");
+// print("Total Rupees=${us+ri}");
+
+//QUESTION NO 9
+ 
+// stdout.write("Enter hours = ");
+// int hr= int.parse(stdin.readLineSync()!);
+// stdout.write("Enter Rate Per hour = ");
+// int rat_pr_hr=int.parse(stdin.readLineSync()!);
+// print("Your Weekly Earning is ${hr*rat_pr_hr}"); 
+
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
